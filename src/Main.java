42f4025540da180bc4d15d3c49eb5451c3d763ef
Julian <jusch1991@gmail.com>
2022-05-12 17:47:00 +0200

    2.0

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import java.time.Duration;
 import java.time.LocalTime;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.TreeSet;
 
 public class Main {
     static Timetable millerStreet = new Timetable();
@@ -17,50 +19,77 @@ public class Main {
         millerStreet.addArrivals(20, Arrays.asList(6, 16, 36));
         millerStreet.addArrivals(Arrays.asList(21, 22, 23), Arrays.asList(16, 36, 56));
 
+        //Generate Timetable with a given start and endtime and an interval
+        millerStreet.addArrivals(LocalTime.of(5,0),  LocalTime.of(23, 0), 18);
 
-        printNextArrival(LocalTime.now().getHour(), LocalTime.now().getMinute());
-        printNextArrival(5, 2);
-        printNextArrival(10, 59);
-        printNextArrival(23, 16);
-        printNextArrival(23, 58);
-        printNextArrival(3, 2);
+        System.out.println("Manuell");
+        printNextArrival(LocalTime.now());
+        printNextArrival(LocalTime.of(5, 2));
+        printNextArrival(LocalTime.of(10, 59));
+        printNextArrival(LocalTime.of(23, 16));
+        printNextArrival(LocalTime.of(23, 58));
+        printNextArrival(LocalTime.of(3, 2));
+
+        System.out.println("Interval");
+        printnextArrivals(LocalTime.of(15, 15), 23, 10);
     }
 
 
-    static void printNextArrival (Integer hour, Integer minute) {
-        System.out.println(LocalTime.of(hour, minute).toString() + " -> " + millerStreet.getNextArrival(hour, minute).toString());
+    static void printNextArrival (LocalTime time) {
+        System.out.println(time.toString() + " -> " + millerStreet.getNextArrival(time.getHour(), time.getMinute()));
+    }
+
+
+    private static void printnextArrivals(LocalTime time, int interval, int numberOfArrivals) {
+        for (int i = 0; i < numberOfArrivals; i++) {
+            printNextArrival(time);
+            time = time.plus(Duration.ofMinutes(interval));
+        }
     }
 
 
     static class Timetable {
-        private HashMap<Integer, List<Integer>> arrivals;
+        private HashMap<Integer, TreeSet<Integer>> arrivals;
 
         public Timetable() {
             arrivals = new HashMap<>();
         }
 
-        public void addArrivals(Integer hour, List<Integer> minutes) {
-            arrivals.put(hour, minutes);
+        public void addArrivals(int hour, List<Integer> minutes) {
+            arrivals.put(hour, new TreeSet<>(minutes));
         }
 
         public void addArrivals(List<Integer>hours, List<Integer> minutes) {
-            for (Integer hour : hours) {
+            for (int hour : hours) {
                 addArrivals(hour, minutes);
             }
         }
 
+        public void addArrivals(LocalTime start, LocalTime end, int interval) {
+            while (start.isBefore(end)) {
+                TreeSet<Integer> minutes = arrivals.get(start.getHour());
+                if (minutes == null) {
+                    minutes = new TreeSet<>();
+                }
+                minutes.add(start.getMinute());
+                arrivals.put(start.getHour(), minutes);
+                start = start.plus(Duration.ofMinutes(interval));
+            }
 
-        public LocalTime getNextArrival(Integer hour, Integer minute) {
+        }
+
+
+        public LocalTime getNextArrival(int hour, int minute) {
             Integer nextMinute = null;
 
             if (hour >= 24) {
                 hour = 0;
             }
 
-            List<Integer> minutes = arrivals.get(hour);
-
+            TreeSet<Integer> minutes = arrivals.get(hour);
             if (minutes != null){
-                nextMinute = minutes.stream().filter(m -> m > minute).findFirst().orElse(null);
+                //nextMinute = minutes.stream().filter(m -> m > minute).findFirst().orElse(null);
+                nextMinute = minutes.higher(minute);
             }
 
             if (nextMinute == null) {
@@ -69,7 +98,6 @@ public class Main {
                 return LocalTime.of(hour, nextMinute);
             }
         }
+
     }
 }
-
-
